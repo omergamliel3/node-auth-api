3c94a15bc3099f75822de8bc1f1df18bc6930140
omergamliel <omerga@prospera.ag>
2021-04-13 11:03:20 +0300

    wait for db connection befire init the app

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -2,7 +2,7 @@ import { createConnection, getConnection } from "typeorm";
 import logger from "@shared/logger";
 
 // Run func when importing file
-const start = async () => {
+export const createDbConnection = async () => {
   try {
     // Checks for existing db connection
     const existingConnection = getConnection();
@@ -13,25 +13,17 @@ const start = async () => {
   } catch (error) {
     logger.info("No DB existing connection");
   }
-  try {
-    // Create Postgres DB connection
-    createConnection({
-      type: "postgres",
-      host: process.env.HOST,
-      port: Number(process.env.DB_PORT || 5432),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ["src/orm/*.ts"],
-      synchronize: true,
-      logging: false,
-    }).then(() => {
-      logger.info("Start Postgres DB connection");
-    });
-  } catch (error) {
-    logger.err(error, true);
-    process.exit(1);
-  }
-};
 
-start();
+  // Create Postgres DB connection
+  return createConnection({
+    type: "postgres",
+    host: process.env.HOST,
+    port: Number(process.env.DB_PORT || 5432),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ["src/orm/*.ts"],
+    synchronize: true,
+    logging: false,
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import "./pre-start"; // Must be the first import
-import "@database";
+import { createDbConnection } from "@database";
 import app from "@server";
 import logger from "@shared/logger";
 
-// Start the server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-  logger.info("Express server started on port: " + port);
-});
+
+createDbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      logger.info("Express server started on port: " + port);
+    });
+  })
+  .catch((err) => {
+    logger.err("Error connecting to DB");
+    logger.err(err, true);
+    process.exit(1);
+  });
