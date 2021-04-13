88d86d31ecfcea3833feb04b694f1f5b363093e1
omergamliel <omerga@prospera.ag>
2021-04-13 12:23:42 +0300

    OOP refactor

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,14 +1,9 @@
+import BaseRoute from "@routes/base.route";
 import { Router } from "express";
 import V1Routes from "./v1/v1.routes";
 
-class ApiRoutes {
-  public router = Router();
-
-  constructor() {
-    this.initializeRoutes();
-  }
-
-  private initializeRoutes() {
+class ApiRoutes extends BaseRoute {
+  initializeRoutes() {
     const v1Routes = new V1Routes();
     this.router.use("/v1", v1Routes.router);
   }
